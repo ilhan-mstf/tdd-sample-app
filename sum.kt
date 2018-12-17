7ae388242d5398573e3e889ad4adb68e9b66ea49
Mustafa ilhan <ilhan.mstf@gmail.com>
2018-12-17 17:54:23 +0300

    Kotlin code removed

--- sum.kt
+++
@@ -1,8 +0,0 @@
-fun main(args: Array<String>) {
-  if (args.size < 1) {
-    println("Error! Please provide at least one integer number")
-    return
-  }
-  val numbers: List<Int> = args.map { it.toInt() }
-  println(numbers.sum())
-}
